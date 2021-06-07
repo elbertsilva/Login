77a70c7b9a7aa64cf53c70b6e225bb756028016d
elbertmartins <elbert.martins@conforce.com.br>
2021-06-07 17:47:16 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Login from './components/Login'
+import Barra from './components/Barra'
+
+const App = () => {
+  return(
+    <>
+   {/* <Login /> */}
+    <Barra />
+
+    </>
+  )
+}
+export default App;
